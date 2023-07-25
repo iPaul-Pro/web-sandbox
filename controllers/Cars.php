a751171972f6f37819fac482fad4a3c52586cc59
Paul <psholloway@me.com>
2023-07-25 18:16:46 +1000

    Tidied up Cars comments

--- controllers/Cars.php
+++ controllers/Cars.php
@@ -1,5 +1,5 @@
 <?php
-	require '../models/Car.php';						// need to important Car class to create Car objects
+	require '../models/Car.php';								// need to important Car class to create Car objects
 
 	class Cars {
 		
@@ -9,29 +9,17 @@
 		public $car_colour;
 		
 		function __construct() {
-    		$this->car = new Car();							// create a car object
-			$this->car->set_make('MERCEDES-BENZ');			// set car make
+    		$this->car = new Car();								// create a car object
+			$this->car->set_make('MERCEDES-BENZ');				// set car make
 			$this->car->set_model('EQA');						// set car model
 			$this->car->set_colour('MOUNTAIN GREY');			// set car colour
   		}
 
 		// Methods
 		function get_car() {
-			return $this->car;							// returns car object
+			return $this->car;									// returns car object
 		}
-		/*
-		function get_car_make() {
-			return $car_make;						// returns variable car_make
-		}
-
-		function get_car_model() {
-			return $car_model;						// returns variable car_model
-		}
-
-		function get_car_colour() {
-			return $car_colour;						// returns variable car_colour
-		}
-		*/
+		
 	}
 
 ?>
