413a4f25b96f43c2967153067995028b3583714d
Paul <psholloway@me.com>
2023-07-25 18:27:36 +1000

    New contructor used to create car object

--- controllers/Cars.php
+++ controllers/Cars.php
@@ -9,10 +9,11 @@
 		public $car_colour;
 		
 		function __construct() {
-    		$this->car = new Car();								// create a car object
-			$this->car->set_make('MERCEDES-BENZ');				// set car make
-			$this->car->set_model('EQA');						// set car model
-			$this->car->set_colour('MOUNTAIN GREY');			// set car colour
+//    		$this->car = new Car();								// create a car object
+//			$this->car->set_make('MERCEDES-BENZ');				// set car make
+//			$this->car->set_model('EQA');						// set car model
+//			$this->car->set_colour('MOUNTAIN GREY');			// set car colour
+			$this->car = new Car("MERCEDES-BENZ", "EQA", "MOUNTAIN GREY");
   		}
 
 		// Methods
