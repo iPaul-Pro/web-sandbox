47c724bc499e7c2f9ea999fb132fa730dc409ff3
Paul <psholloway@me.com>
2023-07-25 16:00:42 +1000

    Updated Cars notes & created controller object

--- views/index.php
+++ views/index.php
@@ -3,7 +3,8 @@
 <head>
 <meta charset="utf-8">
 	<?php
-		require('./controllers/Cars.php');
+		require('../controllers/Cars.php');
+		$cars_controller = new Cars();
 	?>
 <title>Index Page</title>
 </head>
@@ -11,9 +12,9 @@
 <body>
 	<?php echo "Hello World!<br><br>My name is Paul.<br><br>"; ?>
 	<p>
-		Car make: <?php echo get_car_make() ?><br>
-		Car model: <?php echo get_car_model() ?><br>
-		Car colour: <?php echo get_car_colour() ?><br>
+		Car make: <?php echo $cars_controller->get_car_make() ?><br>
+		Car model: <?php echo $cars_controller->get_car_model() ?><br>
+		Car colour: <?php echo $cars_controller->get_car_colour() ?><br>
 	</p>
 </body>
 </html>
