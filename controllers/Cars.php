7095a6d49cc70eeac8504b35e5c716c1ed088baa
Paul <psholloway@me.com>
2023-07-25 18:08:15 +1000

    Converted Cars controller into a class

--- controllers/Cars.php
+++ controllers/Cars.php
@@ -1,34 +1,41 @@
 <?php
-	require '../models/Car.php';				// important Car class to create Car objects
+	require '../models/Car.php';						// need to important Car class to create Car objects
 
-	$car1 = new Car();							// create a car object
-	$car1->set_make('MERCEDES-BENZ');			// set car make
-	$car1->set_model('EQA');						// set car model
-	$car1->set_colour('MOUNTAIN GREY');			// set car colour
+	class Cars {
+		
+		public $car;
+		public $car_make;
+		public $car_model;
+		public $car_colour;
+		
+		function __construct() {
+    		$car = new Car();							// create a car object
+			$car->set_make('MERCEDES-BENZ');			// set car make
+			$car->set_model('EQA');						// set car model
+			$car->set_colour('MOUNTAIN GREY');			// set car colour
+			
+			$car_make = $car1->get_make();				// set variable car_make to make of car
+			$car_model = $car1->get_model();			// set variable car_model to model of car
+			$car_colour = $car1->get_colour();			// set variable car_colour to colour of car
+  		}
 
-//	echo "Make: " . $car1->get_make() . "<br>";
-//	echo "Model: " . $car1->get_model()."<br>";
-//	echo "Colour: " . $car1->get_colour()."<br><br>";
+		// Methods
+		function get_car() {
+			return $this->car;							// returns car object
+		}
+		/*
+		function get_car_make() {
+			return $car_make;						// returns variable car_make
+		}
 
-	$car_make = $car1->get_make();				// set variable car_make to make of car
-	$car_model = $car1->get_model();				// set variable car_model to model of car
-	$car_colour = $car1->get_colour();			// set variable car_colour to colour of car
-	
-//	echo "Make: " . $car_make . "<br>";
-//	echo "Model: " . $car_model ."<br>";
-//	echo "Colour: " . $car_colour ."<br>";
+		function get_car_model() {
+			return $car_model;						// returns variable car_model
+		}
 
-	// Methods
-	function get_car_make() {
-		return $car_make;						// returns variable car_make
-	}
-
-	function get_car_model() {
-		return $car_model;						// returns variable car_model
-	}
-
-	function get_car_colour() {
-		return $car_colour;						// returns variable car_colour
+		function get_car_colour() {
+			return $car_colour;						// returns variable car_colour
+		}
+		*/
 	}
 
 ?>
