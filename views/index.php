e446f840321f632e65a811619d519fc02db30edf
Paul <psholloway@me.com>
2023-07-25 15:46:16 +1000

    Added car properties to index page

--- views/index.php
+++ views/index.php
@@ -2,10 +2,18 @@
 <html>
 <head>
 <meta charset="utf-8">
+	<?php
+		require('./controllers/Cars.php');
+	?>
 <title>Index Page</title>
 </head>
 
 <body>
-	<?php echo "Hello World!<br>My name is Paul."; ?>
+	<?php echo "Hello World!<br><br>My name is Paul.<br><br>"; ?>
+	<p>
+		Car make: <?php echo get_car_make() ?><br>
+		Car model: <?php echo get_car_model() ?><br>
+		Car colour: <?php echo get_car_colour() ?><br>
+	</p>
 </body>
 </html>
