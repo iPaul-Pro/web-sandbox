21e435d768f2d7072092c86a6a925c48448aed18
Paul <psholloway@me.com>
2023-07-26 10:20:18 +1000

    Created form to update car details

---
+++ views/car-form.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Car Form</title>
+</head>
+
+<body>
+	<p>
+		CAR FORM<br><br>
+		Use this form to change the details of the car.<br><br>
+		<form action="#update_car" target="_self" method="get">
+			<label for="make">Make:</label><br>
+			<input type="text" id="make" name="make"><br><br>
+			<label for="model">Model:</label><br>
+			<input type="text" id="model" name="model"><br><br>
+			<label for="colour">Colour:</label><br>
+			<input type="text" id="colour" name="colour"><br><br>
+			<input type="submit" value="Submit">
+		</form>
+	</p>
+</body>
+</html>
