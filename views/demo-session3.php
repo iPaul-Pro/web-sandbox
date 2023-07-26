a0f264480f3e4dc8255deb1d07d9d4b42f97e23b
Paul <psholloway@me.com>
2023-07-26 19:28:28 +1000

    Amended validation method

--- views/demo-session3.php
+++ views/demo-session3.php
@@ -12,7 +12,10 @@ session_start();
 	<?php
 		// to change a session variable, just overwrite it
 		$_SESSION["favcolor"] = "yellow";
-		print_r($_SESSION);
+		//print_r($_SESSION);
+		// Echo session variables that were set on previous page
+		echo "Favorite colour is " . $_SESSION["favcolor"] . ".<br>";
+		echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
 	?>
 </body>
 </html>
